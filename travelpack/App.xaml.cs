821881fa476e000745daf2d6fc161f61f04375ed
Mohammed Sadiq <>
2020-04-30 04:03:28 +0100

    no message

---
+++ travelpack/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using travelpack.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesomePro-Light.otf", Alias = "FontAwesomeLight")]
+[assembly: ExportFont("FontAwesomePro-Regular.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("FontAwesomePro-Solid.otf", Alias = "FontAwesomeSolid")]
+[assembly: ExportFont("OpenSans-Bold.ttf", Alias = "OpenSansBold")]
+[assembly: ExportFont("OpenSans-BoldItalic.ttf", Alias = "OpenSansBoldItalic")]
+[assembly: ExportFont("OpenSans-ExtraBold.ttf", Alias = "OpenSansExtraBold")]
+[assembly: ExportFont("OpenSans-ExtraBoldItalic.ttf", Alias = "OpenSansExtraBoldItalic")]
+[assembly: ExportFont("OpenSans-Italic.ttf", Alias = "OpenSansItalic")]
+[assembly: ExportFont("OpenSans-Light.ttf", Alias = "OpenSansLight")]
+[assembly: ExportFont("OpenSans-LightItalic.ttf", Alias = "OpenSansLightItalic")]
+[assembly: ExportFont("OpenSans-Regular.ttf", Alias = "OpenSansRegular")]
+[assembly: ExportFont("OpenSans-Semibold.ttf", Alias = "OpenSansSemibold")]
+[assembly: ExportFont("OpenSans-SemiboldItalic.ttf", Alias = "OpenSansSemiboldItalic")]
+
+namespace travelpack
+{
+    public partial class App
+    {
+        /*
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor.
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ travelpack/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace travelpack.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService, dialogService)
+        {
+        }
+    }
+}
